08d8a295b16a394681cdb903d81a62e2effc9733
Ihar Iuliyeu <iiuliyeu@scnsoft.com>
2019-09-16 17:06:22 +0300

    Updete comments

--- test/test_login.py
+++ test/test_login.py
@@ -8,7 +8,7 @@ import pytest
 def test_login(app, config):
     with pytest.allure.step('Open Login Page and fill in fields "Username" and "Password", click "Login'):
         app.login_page.enter_login_information(config['web']['username'], config['web']['password'])
-    with pytest.allure.step('Verify text on "Secure area" page'):
+    with pytest.allure.step('Verify sub-header text for "Secure area" page'):
         assert app.secure_area_page.check_secure_area_text(
             'Welcome to the Secure Area. When you are done click logout below.')
     with pytest.allure.step('Click "Logout" button'):
