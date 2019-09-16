38998ca91a53321e707728e1cb37cad86c2b6f1b
Ihar Iuliyeu <iiuliyeu@scnsoft.com>
2019-09-16 12:33:07 +0300

    Add comments into test

--- test/test_login.py
+++ test/test_login.py
@@ -1,6 +1,10 @@
 import pytest
 
 
+######################################
+# Test page "Secure Area" #
+######################################
+
 def test_login(app, config):
     with pytest.allure.step('Open Login page and fill in login and password'):
         app.session.login(config['web']['username'], config['web']['password'])
@@ -10,4 +14,3 @@ def test_login(app, config):
         assert app.session.is_logged_in_as('Welcome to the Secure Area. When you are done click logout below.')
     with pytest.allure.step('Click button logout'):
         app.session.logout()
-
